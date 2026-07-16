3603c0fb0d8da49ce9834158a4916daa6bfae108
nitesh-20 <nsahu6156@gmail.com>
2026-07-16 15:40:18 +0530

    style: load global CSS stylesheets and implement Annam.ai minimalist dark glowing aesthetic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
